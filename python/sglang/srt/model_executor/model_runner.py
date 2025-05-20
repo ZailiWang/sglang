b00b9aa560c90e4b21681105f832a7c91d412494
Chunyuan WU <chunyuan.wu@intel.com>
2025-05-20 12:46:29 +0800

    De-couple vLLM CPU and remove dependency of `sudo apt-get install libnuma-dev` (#74)
    * port utils.cpp for numa binding from vllm into sglang
    
    * fix build
    
    * add pybind for init_cpu_threads_env
    
    * use conda prefix to find libnuma # how to ensure libnuma is installed via conda
    
    * replace init_cpu_threads_env in vllm with that in sgl-kernel
    
    * add vllm into srt_cpu
    
    * fix format

--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -469,9 +469,11 @@ class ModelRunner:
         set_custom_all_reduce(not self.server_args.disable_custom_all_reduce)
 
         if not self.is_draft_worker:
-            # Bind OpenMP threads to CPU cores
-            if self.device == "cpu" and self.local_omp_cpuid != "all":
-                torch.ops._C_utils.init_cpu_threads_env(self.local_omp_cpuid)
+            if self.device == "cpu":
+                import sgl_kernel.common_ops
+                # Bind OpenMP threads to CPU cores
+                if self.local_omp_cpuid != "all":
+                    sgl_kernel.common_ops.init_cpu_threads_env(self.local_omp_cpuid)
 
             # Only initialize the distributed environment on the target model worker.
             init_distributed_environment(
